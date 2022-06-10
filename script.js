54150a007fb242f70ad54d30ac7a18b1089aa47e
79139 <vizdeneva@gmail.com>
2022-06-10 09:32:55 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,102 @@
+/*let data = {"pagination":{"last_visible_page":14,"has_next_page":true,"current_page":1,"items":{"count":2,"total":27,"per_page":2}},
+    "data":[{"mal_id":20,
+                "url":"https:\/\/myanimelist.net\/anime\/20\/Naruto",
+                "images":{
+                    "jpg":{"image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405.jpg","small_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405t.jpg","large_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405l.jpg"},
+                    "webp":{"image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405.webp","small_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405t.webp","large_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/13\/17405l.webp"}},
+                "trailer":{"youtube_id":null,"url":null,"embed_url":null,"images":{"image_url":null,"small_image_url":null,"medium_image_url":null,"large_image_url":null,"maximum_image_url":null}},
+                "title":"Naruto","title_english":"Naruto","title_japanese":"\u30ca\u30eb\u30c8","title_synonyms":["NARUTO"],"type":"TV","source":"Manga","episodes":220,"status":"Finished Airing","airing":false,"aired":{"from":"2002-10-03T00:00:00+00:00","to":"2007-02-08T00:00:00+00:00","prop":{"from":{"day":3,"month":10,"year":2002},"to":{"day":8,"month":2,"year":2007}},"string":"Oct 3, 2002 to Feb 8, 2007"},"duration":"23 min per ep","rating":"PG-13 - Teens 13 or older","score":7.97,"scored_by":1728231,"rank":605,"popularity":8,"members":2476994,"favorites":69544,"synopsis":"Moments prior to Naruto Uzumaki's birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubi's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto. Now, Naruto is a hyperactive and knuckle-headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes. [Written by MAL Rewrite]","background":"Naruto received numerous awards during its airing, including the \"Best Full-Length Animation Program Award\" in the Third UStv Awards and the 38th \"Best Animated Show\" in IGN's Top 100 Animated Series.","season":"fall","year":2002,"broadcast":{"day":"Thursdays","time":"19:30","timezone":"Asia\/Tokyo","string":"Thursdays at 19:30 (JST)"},"producers":[{"mal_id":16,"type":"anime","name":"TV Tokyo","url":"https:\/\/myanimelist.net\/anime\/producer\/16\/TV_Tokyo"},{"mal_id":17,"type":"anime","name":"Aniplex","url":"https:\/\/myanimelist.net\/anime\/producer\/17\/Aniplex"},{"mal_id":1365,"type":"anime","name":"Shueisha","url":"https:\/\/myanimelist.net\/anime\/producer\/1365\/Shueisha"}],"licensors":[{"mal_id":119,"type":"anime","name":"VIZ Media","url":"https:\/\/myanimelist.net\/anime\/producer\/119\/VIZ_Media"}],"studios":[{"mal_id":1,"type":"anime","name":"Studio Pierrot","url":"https:\/\/myanimelist.net\/anime\/producer\/1\/Studio_Pierrot"}],"genres":[{"mal_id":1,"type":"anime","name":"Action","url":"https:\/\/myanimelist.net\/anime\/genre\/1\/Action"},{"mal_id":2,"type":"anime","name":"Adventure","url":"https:\/\/myanimelist.net\/anime\/genre\/2\/Adventure"},{"mal_id":10,"type":"anime","name":"Fantasy","url":"https:\/\/myanimelist.net\/anime\/genre\/10\/Fantasy"}],"explicit_genres":[],"themes":[{"mal_id":17,"type":"anime","name":"Martial Arts","url":"https:\/\/myanimelist.net\/anime\/genre\/17\/Martial_Arts"}],"demographics":[{"mal_id":27,"type":"anime","name":"Shounen","url":"https:\/\/myanimelist.net\/anime\/genre\/27\/Shounen"}]},
+            {"mal_id":32365,
+                "url":"https:\/\/myanimelist.net\/anime\/32365\/Boruto__Naruto_the_Movie_-_Naruto_ga_Hokage_ni_Natta_Hi",
+                "images":{
+                    "jpg":{"image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678.jpg","small_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678t.jpg","large_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678l.jpg"},
+                    "webp":{"image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678.webp","small_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678t.webp","large_image_url":"https:\/\/cdn.myanimelist.net\/images\/anime\/6\/77678l.webp"}},
+                "trailer":{"youtube_id":null,"url":null,"embed_url":null,"images":{"image_url":null,"small_image_url":null,"medium_image_url":null,"large_image_url":null,"maximum_image_url":null}},
+                "title":"Boruto: Naruto the Movie - Naruto ga Hokage ni Natta Hi","title_english":"Boruto: Naruto the Movie - The Day Naruto Became Hokage","title_japanese":"BORUTO -NARUTO THE MOVIE- \u30ca\u30eb\u30c8\u304c\u706b\u5f71\u306b\u306a\u3063\u305f\u65e5","title_synonyms":["The Day Naruto Became Hokage"],"type":"Special","source":"Original","episodes":1,"status":"Finished Airing","airing":false,"aired":{"from":"2016-07-06T00:00:00+00:00","to":null,"prop":{"from":{"day":6,"month":7,"year":2016},"to":{"day":null,"month":null,"year":null}},"string":"Jul 6, 2016"},"duration":"10 min","rating":"PG-13 - Teens 13 or older","score":7.36,"scored_by":55945,"rank":2200,"popularity":1762,"members":98644,"favorites":133,"synopsis":"Bundled with the limited edition of Blu-ray\/DVD of Boruto: Naruto the Movie.","background":null,"season":null,"year":null,"broadcast":{"day":null,"time":null,"timezone":null,"string":null},"producers":[],"licensors":[{"mal_id":119,"type":"anime","name":"VIZ Media","url":"https:\/\/myanimelist.net\/anime\/producer\/119\/VIZ_Media"}],"studios":[{"mal_id":1,"type":"anime","name":"Studio Pierrot","url":"https:\/\/myanimelist.net\/anime\/producer\/1\/Studio_Pierrot"}],"genres":[{"mal_id":1,"type":"anime","name":"Action","url":"https:\/\/myanimelist.net\/anime\/genre\/1\/Action"},{"mal_id":2,"type":"anime","name":"Adventure","url":"https:\/\/myanimelist.net\/anime\/genre\/2\/Adventure"},{"mal_id":10,"type":"anime","name":"Fantasy","url":"https:\/\/myanimelist.net\/anime\/genre\/10\/Fantasy"}],"explicit_genres":[],"themes":[],"demographics":[{"mal_id":27,"type":"anime","name":"Shounen","url":"https:\/\/myanimelist.net\/anime\/genre\/27\/Shounen"}]}]};*/
+
+let data;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const spinner = document.getElementById("spinner");
+    const search = document.getElementById("search");
+    const form = document.getElementById("inputForm");
+    const fltr = document.getElementById("fltr");
+    const sort = document.getElementById("sort");
+    const up = document.getElementById("up");
+    const down = document.getElementById("down");
+    const input = document.getElementById("input");
+    form.addEventListener("submit",   async e => {
+        e.preventDefault();
+
+        spinner.className = "spinnerOn";
+        search.disabled = true;
+        const input = document.getElementById("input");
+
+        const response = await fetch(`https://api.jikan.moe/v4/anime?q=${input.value}&type=${fltr.value}order_by=${sort.value}`);
+        data = await response.json();
+
+        spinner.className = "spinnerOff";
+        search.disabled = false;
+        render();
+    })
+    fltr.addEventListener("change", async e => {
+        e.preventDefault();
+
+        spinner.className = "spinnerOn";
+        search.disabled = true;
+        const input = document.getElementById("input");
+
+        const response = await fetch(`https://api.jikan.moe/v4/anime?q=${input.value}&type=${fltr.value}&order_by=${sort.value}&sort=asc`);
+        data = await response.json();
+
+        spinner.className = "spinnerOff";
+        search.disabled = false;
+        render();
+    })
+    sort.addEventListener("change", async e =>{
+        e.preventDefault();
+
+        up.addEventListener("click", async e => {
+            e.preventDefault();
+
+            spinner.className = "spinnerOn";
+            search.disabled = true;
+
+            const response = await fetch(`https://api.jikan.moe/v4/anime?q=${input.value}&order_by=${sort.value}&sort=asc&type=${fltr.value}`);
+            data = await response.json();
+
+            spinner.className = "spinnerOff";
+            search.disabled = false;
+            render();
+        })
+        down.addEventListener("click", async e => {
+            e.preventDefault();
+
+            spinner.className = "spinnerOn";
+            search.disabled = true;
+
+            const response = await fetch(`https://api.jikan.moe/v4/anime?q=${input.value}&order_by=${sort.value}&sort=desc&sort=desc&type=${fltr.value}`);
+            data = await response.json();
+
+            spinner.className = "spinnerOff";
+            search.disabled = false;
+            render();
+        })
+    })
+});
+
+function render() {
+    const container = document.getElementById("container");
+    let result = "";
+
+    for (let item of data.data) {
+        result += `<div class="divAnime">
+            <img class="cover" width="80%" height="70%" src="${item.images.jpg.image_url}">
+            <p class="tittle">${item.title}</p>
+        </div>`;
+    }
+
+    container.innerHTML = result;
+}
+
+
